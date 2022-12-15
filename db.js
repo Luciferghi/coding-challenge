127d3e2a0c4ffb101224771f4fc7f784179e4c45
ShreeKrishna <nitish.ghimire1@gmail.com>
2022-12-15 02:33:22 -0800

    updated the mongodb for online

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
-    mongoose.connect('mongodb://127.0.0.1:27017/codingchallenge',).then(() => {
+    mongoose.connect('mongodb+srv://challenge:challenge@cluster0.z7wg1th.mongodb.net/?retryWrites=true&w=majority',).then(() => {
         console.log('Mongodb Connected');
     })
 }
